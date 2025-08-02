319a45a2932d1e61bc6ef81659d1911e8b2ed011
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-08-02 13:32:14 -0400

    Inicio

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -13,14 +13,14 @@ const Inicio = () => {
     <div className="relative p-6 text-center min-h-screen bg-white">
       {/* Botón en la esquina superior derecha */}
       <div className="absolute top-4 right-4">
-        <Boton texto="Ver Mis Listas" onClick={irAListas} />
+        <Boton texto="Listas" onClick={irAListas} />
       </div>
 
       <h1 className="text-3xl font-bold mb-4 text-gray-800 mt-12">
         Bienvenido a TaskBolt
       </h1>
       <h2 className="text-sky-600 hover:text-sky-700 transition-colors duration-200 mb-6">
-        Gestiona tus listas de compras aquí.
+        Gestiona tus listas de compras aquí. aquí
       </h2>
 
       {/* Imagen responsive visible en móvil, 75% del ancho */}
